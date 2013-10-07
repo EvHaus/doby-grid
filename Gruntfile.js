f39a7afd373562ebe8970ca759e4be479d567ff7
Evgueni Naverniouk <globexdesigns@gmail.com>
2013-10-07 09:55:11 -0700

    Adding theme support

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,14 @@
 module.exports = function (grunt) {
 	'use strict';
 
-	// Project configuration.
+	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 
-		jshint: {
-			ignore_warning: {
-				options: {
-					'-W083': true,
-					'-W030': true
-				},
-				src: ['src/<%= pkg.name %>.js']
+		copy: {
+			main: {
+				src: "src/<%= pkg.name %>.js",
+				dest: "build/<%= pkg.version %>/<%= pkg.name %>.js"
 			}
 		},
 
@@ -32,9 +29,32 @@ module.exports = function (grunt) {
 			}
 		},
 
+		jshint: {
+			ignore_warning: {
+				options: {
+					'-W083': true,
+					'-W030': true
+				},
+				src: ['src/<%= pkg.name %>.js']
+			}
+		},
+
+		less: {
+			options: {
+				yuicompress: true
+			},
+			production: {
+				files: {
+					'build/<%= pkg.version %>/<%= pkg.name %>.min.css': 'src/<%= pkg.name %>.less',
+					'build/<%= pkg.version %>/themes/<%= pkg.name %>-light.css': 'src/themes/<%= pkg.name %>-light.less',
+					'build/<%= pkg.version %>/themes/<%= pkg.name %>-dark.css': 'src/themes/<%= pkg.name %>-dark.less'
+				}
+			}
+		},
+
 		uglify: {
 			options: {
-				banner: '// doby-grid.js 0.0.1\n\
+				banner: '// <%= pkg.name %>.js <%= pkg.version %>\n\
 // (c) 2013 Evgueni Naverniouk, Globex Designs, Inc.\n\
 // Doby may be freely distributed under the MIT license.\n\
 // For all details and documentation:\n\
@@ -43,29 +63,23 @@ module.exports = function (grunt) {
 					except: ['jQuery', 'Backbone', '_']
 				}
 			},
+
 			build: {
 				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.version %>/<%= pkg.name %>-<%= pkg.version %>.min.js'
-			}
-		},
-
-		less: {
-			production: {
-				files: {
-					'build/<%= pkg.version %>/<%= pkg.name %>-<%= pkg.version %>.min.css': 'src/<%= pkg.name %>.less'
-				}
+				dest: 'build/<%= pkg.version %>/<%= pkg.name %>.min.js'
 			}
 		}
 	});
 
 	// Load plugins
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Run tasks.
 	// TODO: jasmine is temporarily disabled because it doesn't behave
 	// as expected when testing the rendering of elements
-	grunt.registerTask('default', ['jshint', 'uglify', 'less']);
+	grunt.registerTask('default', ['jshint', 'uglify', 'less', 'copy']);
 };
