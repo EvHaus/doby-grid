14b074833524190e157c67e1fb8f92714a53c98f
Evgueni Naverniouk <globexdesigns@gmail.com>
2014-01-15 16:17:37 -0800

    Change Aggregators example data to something more easily readable.

--- examples/aggregators.js
+++ examples/aggregators.js
@@ -4,6 +4,46 @@
 define([], function () {
 	"use strict";
 
+	var default_aggregators = [{
+		name: "Average",
+		fn: function (column) {
+			this.total = [];
+			this.exporter = function () {
+				var avg;
+				if (this.total.length) {
+					avg = this.total.reduce(function (a, b) { return a + b; });
+				}
+				return Math.round(avg / this.total.length);
+			};
+			this.formatter = function () {
+				var avg;
+				if (this.total.length) {
+					avg = this.total.reduce(function (a, b) { return a + b; });
+				}
+				return "Avg: <strong>" + Math.round(avg / this.total.length) + "</strong>";
+			};
+			this.process = function (item) {
+				this.total.push(item.data[column.field]);
+			};
+			return this;
+		},
+	}, {
+		name: "Total",
+		fn: function (column) {
+			this.total = 0;
+			this.exporter = function () {
+				return (this.total || "");
+			};
+			this.formatter = function () {
+				return "Total: <strong>" + this.total + "</strong>";
+			};
+			this.process = function (item) {
+				this.total += (item.data[column.field] || 0);
+			};
+			return this;
+		}
+	}];
+
 	// Generate Grid Options
 	return [function () {
 
@@ -14,87 +54,46 @@ define([], function () {
 				id: i,
 				data: {
 					id: i,
-					population: _.sample([15, 24, 14, 62, 12, 1, 100]),
+					population: _.sample(_.range(15, 100)),
+					elevation: _.sample(_.range(500, 10000)),
+					language: _.sample(["English", "French", "Italian", "Latin", null]),
 					city: ['Venice', 'Vatican City', 'Rome', 'Milan', 'Constantinople'][_.random(0, 4)]
 				}
 			});
 		}
 
 		// Generate Columns
-		var columns = [];
-		for (var q = 0; q < 5; q++) {
-			columns.push({
-				aggregators: [{
-					name: "Average",
-					fn: function () {
-						this.total = [];
-						this.exporter = function () {
-							var avg;
-							if (this.total.length) {
-								avg = this.total.reduce(function (a, b) { return a + b; });
-							}
-							return Math.round(avg / this.total.length);
-						};
-						this.formatter = function () {
-							var avg;
-							if (this.total.length) {
-								avg = this.total.reduce(function (a, b) { return a + b; });
-							}
-							return "Avg: <strong>" + Math.round(avg / this.total.length) + "</strong>";
-						};
-						this.process = function (item) {
-							this.total.push(item.id);
-						};
-						return this;
-					},
-				}, {
-					name: "Hello World",
-					fn: function () {
-						this.exporter = function () {
-							return "Hello World";
-						};
-						this.formatter = function () {
-							return "Hello World";
-						};
-						this.process = function () {
-							return;
-						};
-						return this;
-					}
-				}],
-				id: "id" + q,
-				name: "ID",
-				field: "id",
-				sortable: true
-			}, {
-				aggregators: [{
-					name: "Total",
-					fn: function (column) {
-						this.total = 0;
-						this.exporter = function () {
-							return (this.total || "");
-						};
-						this.formatter = function () {
-							return "Total: <strong>" + this.total + "</strong>";
-						};
-						this.process = function (item) {
-							this.total += (item.data[column.field] || 0);
-						};
-						return this;
-					}
-				}],
-				id: "population" + q,
-				name: "Population",
-				field: "population",
-				minWidth: 100,
-				sortable: true
-			}, {
-				id: "city" + q,
-				name: "City",
-				field: "city",
-				sortable: true
-			});
-		}
+		var columns = [{
+			id: "id",
+			name: "ID",
+			field: "id",
+			sortable: true
+		}, {
+			id: "city",
+			name: "City",
+			field: "city",
+			minWidth: 160,
+			sortable: true
+		}, {
+			aggregators: default_aggregators,
+			id: "elevation",
+			name: "Elevation",
+			field: "elevation",
+			minWidth: 100,
+			sortable: true
+		}, {
+			aggregators: default_aggregators,
+			id: "population",
+			name: "Population",
+			field: "population",
+			minWidth: 100,
+			sortable: true
+		}, {
+			id: "language",
+			name: "Language",
+			field: "language",
+			sortable: true
+		}];
 
 		return {
 			columns: columns,
@@ -102,6 +101,6 @@ define([], function () {
 			quickFilter: true
 		};
 	}, function (grid) {
-		grid.addGrouping('city1', {collapsed: false});
+		grid.addGrouping('city', {collapsed: false});
 	}];
 });
