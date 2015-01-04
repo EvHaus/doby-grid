df7123afb3e53456e10196231af0257d79ea557d
Evgueni Naverniouk <globexdesigns@gmail.com>
2015-01-04 14:23:40 -0800

    Split locale strings to a new file

--- src/doby-grid.js
+++ src/doby-grid.js
@@ -30,6 +30,7 @@ var Aggregate			= require('./classes/Aggregate'),
 	Tooltip				= require('./classes/Tooltip'),
 
 	CLS					= require('./utils/classes'),
+	Locales				= require('./utils/locales'),
 	naturalSort			= require('./utils/naturalSort'),
 	removeElement 		= require('./utils/removeElement');
 
@@ -293,55 +294,7 @@ var DobyGrid = function (options) {
 
 	// A recursive extend for locale options.
 	options.locale = options.locale || {};
-	options.locale = $.extend(true, {
-		column: {
-			add_group:			'Add Grouping By "{{name}}"',
-			add_sort_asc:		'Add Sort By "{{name}}" (Ascending)',
-			add_sort_desc:		'Add Sort By "{{name}}" (Descending)',
-			add_quick_filter:	'Add Quick Filter...',
-			aggregators:		'Aggregators',
-			filter:				'Quick Filter by "{{name}}"',
-			filtering:			'Filtering',
-			group:				'Group By "{{name}}"',
-			grouping:			'Grouping',
-			groups_clear:		'Clear All Grouping',
-			groups_collapse:	'Collapse All Groups',
-			groups_expand:		'Expand All Groups',
-			options:			'Column Options',
-			remove:				'Remove "{{name}}" Column',
-			remove_group:		'Remove Grouping By "{{name}}"',
-			remove_sort:		'Remove Sort By "{{name}}"',
-			sorting:			'Sorting',
-			sort_asc:			'Sort By "{{name}}" (Ascending)',
-			sort_desc:			'Sort By "{{name}}" (Descending)'
-		},
-		empty: {
-			"default":			'No data available',
-			remote:				'No results found',
-			filter:				'No items matching that filter'
-		},
-		global: {
-			auto_width:			'Automatically Resize Columns',
-			columns:			'Columns',
-			export:				'Export',
-			export_csv:			'Export Table to CSV',
-			export_html:		'Export Table to HTML',
-			export_group_csv:	'Export Group to CSV',
-			export_group_html:	'Export Group to HTML',
-			extensions:			'Extensions',
-			grid_options:		'Grid Options',
-			hide_filter:		'Hide Quick Filter'
-		},
-		selection: {
-			deselect_all:		'Deselect All',
-			deselect_cell:		'Deselect This Cell',
-			deselect_column:	'Deselect "{{name}}" Column',
-			selection:			'Selection',
-			select_all:			'Select All',
-			select_cell:		'Select This Cell',
-			select_column:		'Select "{{name}}" Column'
-		}
-	}, options.locale);
+	options.locale = $.extend(true, Locales, options.locale);
 
 	// But simple extend for these options
 	// We can't make all options recursively extended because options.data could
---
+++ src/utils/locales.js
@@ -0,0 +1,49 @@
+module.exports = {
+	column: {
+		add_group:			'Add Grouping By "{{name}}"',
+		add_sort_asc:		'Add Sort By "{{name}}" (Ascending)',
+		add_sort_desc:		'Add Sort By "{{name}}" (Descending)',
+		add_quick_filter:	'Add Quick Filter...',
+		aggregators:		'Aggregators',
+		filter:				'Quick Filter by "{{name}}"',
+		filtering:			'Filtering',
+		group:				'Group By "{{name}}"',
+		grouping:			'Grouping',
+		groups_clear:		'Clear All Grouping',
+		groups_collapse:	'Collapse All Groups',
+		groups_expand:		'Expand All Groups',
+		options:			'Column Options',
+		remove:				'Remove "{{name}}" Column',
+		remove_group:		'Remove Grouping By "{{name}}"',
+		remove_sort:		'Remove Sort By "{{name}}"',
+		sorting:			'Sorting',
+		sort_asc:			'Sort By "{{name}}" (Ascending)',
+		sort_desc:			'Sort By "{{name}}" (Descending)'
+	},
+	empty: {
+		"default":			'No data available',
+		remote:				'No results found',
+		filter:				'No items matching that filter'
+	},
+	global: {
+		auto_width:			'Automatically Resize Columns',
+		columns:			'Columns',
+		export:				'Export',
+		export_csv:			'Export Table to CSV',
+		export_html:		'Export Table to HTML',
+		export_group_csv:	'Export Group to CSV',
+		export_group_html:	'Export Group to HTML',
+		extensions:			'Extensions',
+		grid_options:		'Grid Options',
+		hide_filter:		'Hide Quick Filter'
+	},
+	selection: {
+		deselect_all:		'Deselect All',
+		deselect_cell:		'Deselect This Cell',
+		deselect_column:	'Deselect "{{name}}" Column',
+		selection:			'Selection',
+		select_all:			'Select All',
+		select_cell:		'Select This Cell',
+		select_column:		'Select "{{name}}" Column'
+	}
+};
