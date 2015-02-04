79d793023650c445bef15538f5a0d938d20620b5
Christian Kosmowski <ksmwsk@gmail.com>
2015-02-04 10:18:06 +0100

    Fixed problem with undefined cell nodes

--- src/doby-grid.js
+++ src/doby-grid.js
@@ -4265,7 +4265,8 @@ var DobyGrid = function (options) {
 			// Do not allow negative values
 			nodecell = nodecell < 0 ? 0 : nodecell;
 
-			return cache.nodes[row].cellNodesByColumnIdx[nodecell][0];
+			var cellNodeArray = cache.nodes[row].cellNodesByColumnIdx[nodecell];
+			return cellNodeArray && cache.nodes[row].cellNodesByColumnIdx[nodecell][0];
 		}
 		return null;
 	};
